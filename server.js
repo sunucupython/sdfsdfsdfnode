599db76bdc7a03bb8004fe5f7b486200440afd4c
sunucupython <44699541+sunucupython@users.noreply.github.com>
2019-11-19 22:38:51 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http').createServer();
+const io = require('socket.io')(http);
+
+http.listen(3000);
+
+let count = 0;
+let activeColor = '#f1f1f1';
+console.log('basladsim user');
+
+
+io.on('connection', socket => {
+    console.log('new user');
+    count++;
+    io.emit('newUser',count);
+    socket.emit('changeColor',activeColor);
+
+    socket.on("changeColor",color =>{
+        activeColor = color;
+        socket.broadcast.emit("changeColor",activeColor);
+
+    })
+    socket.on('disconnect',() =>{
+        count--;
+        io.emit('disUser',count);
+        console.log("disconnect kullanici");
+    })
+});
