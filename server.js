37cc4e66f59af4121bd2e847e1b6fb67dc613927
sunucupython <44699541+sunucupython@users.noreply.github.com>
2019-11-19 23:19:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const http = require('http').createServer();
 const io = require('socket.io')(http);
-const PORT = process.env.PORT || 3000;
-const host = '0.0.0.0';
-http.listen(PORT,host);
+const PORT = process.env.PORT
+http.listen(PORT);
 
 let count = 0;
 let activeColor = '#f1f1f1';
